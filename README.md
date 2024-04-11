1bd683a9ad30954f40909be742f3d81ef77cade6
bcordovag <lcordova@roo.com.gt>
2024-04-10 23:14:14 -0600

    creating services

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # myFirstApp-NestJS
+
 My First App using Nest JS
+
+nest g mo name-of-module <!-- nest g mo auth --no-spec -->
+nest g co name-of-controller <!-- nest g co auth --no-spec -->
+nest g s name-of-service<!-- nest g s auth --no-spec -->
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller({})
 export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
   @Get('/tasks')
   getAllTasks() {
-    return 'Getting All Tasks';
+    return this.tasksService.getTasks();
   }
 }
