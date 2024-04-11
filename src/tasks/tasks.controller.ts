2c14329581e25ff81bfccf213c453761218f46f6
bcordovag <lcordova@roo.com.gt>
2024-04-11 10:20:26 -0600

    HTTP Methods

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
-@Controller({})
+@Controller('/tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
-  @Get('/tasks')
+  @Get()
   getAllTasks() {
     return this.tasksService.getTasks();
+  }
+
+  @Post()
+  createTask() {
+    return this.tasksService.createTask();
+  }
+
+  // Put is an update of the entire object.
+  @Put()
+  updateTask() {
+    return this.tasksService.updateTask();
+  }
+
+  // Patch is a partial update.
+  @Patch()
+  updateTaskStatus() {
+    return this.tasksService.updateTaskStatus();
+  }
+
+  @Delete()
+  deleteTask() {
+    return this.tasksService.deleteTask();
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,4 +5,20 @@ export class TasksService {
   getTasks() {
     return ['Task 1', 'Task 2', 'Task 3'];
   }
+
+  createTask() {
+    return 'Creating Tasks';
+  }
+
+  updateTask() {
+    return 'Creating Tasks';
+  }
+
+  updateTaskStatus() {
+    return 'Updating the status of a Task';
+  }
+
+  deleteTask() {
+    return 'Deleting Tasks';
+  }
 }
