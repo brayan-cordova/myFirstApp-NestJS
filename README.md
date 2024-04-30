9f16e9270f9565e9ac1b0b86f2358dfb5cafb2a9
bcordovag <lcordova@roo.com.gt>
2024-04-29 22:38:08 -0600

    Connecting database with Prisma, PostgreSQL and Docker

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ My First App using Nest JS
 nest g mo name-of-module <!-- nest g mo auth --no-spec -->
 nest g co name-of-controller <!-- nest g co auth --no-spec -->
 nest g s name-of-service<!-- nest g s auth --no-spec -->
+
+npm install prisma --save-dev <!-- Install Prisma -->
+npx prisma init <!-- Initialize Prisma -->
+npx prisma migrate dev --name init <!-- Migrate the Database -->
+
+Docker
+docker compose up -d <!-- Launch a PostgreSQL Database -->
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,30 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UsersService {
-  private users: any[] = [
-    {
-      id: 1,
-      name: 'John Doe',
-      phone: '1234567890',
-    },
-    {
-      id: 2,
-      name: 'Jane Miller',
-      phone: '0987654321',
-    },
-  ];
+  // prisma connection
+  constructor(private prisma: PrismaService) {}
 
   getUsers() {
-    return this.users;
+    return this.prisma.user.findMany();
   }
 
   createUser(user: CreateUserDto) {
-    this.users.push(user);
-    return {
-      ...user,
-      id: this.users.length + 1,
-    };
+    return this.prisma.user.create({ data: user });
   }
 }
